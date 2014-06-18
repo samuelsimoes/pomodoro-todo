0a0ee46797a60f5e89828b7a310fc814877cd698
Samuel Simões <slip.cm@gmail.com>
2014-06-17 23:50:58 -0300

    Javascript por controller
    Este commit adiciona a lógica de um arquivo javascript por controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   self.responder = ApplicationResponder
+
+  def javascript_exists?
+    script = "#{Rails.root}/app/assets/javascripts/#{controller_path}.js"
+    File.exists?(script)
+  end
+  helper_method :javascript_exists?
 end
