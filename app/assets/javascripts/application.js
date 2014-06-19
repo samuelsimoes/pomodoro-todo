74289573c8973a66ad3af3f0b02c51ac3e061f28
Samuel Simões <slip.cm@gmail.com>
2014-06-19 11:17:56 -0300

    Adicionado o Angular.js no projeto

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
 //= require jquery
 //= require jquery_ujs
+//= require angular
+
+angular.module("Pomodoro", []);
+angular.module("PomodoroApp", ["Pomodoro"]).config([
+  "$httpProvider",
+  function ($httpProvider) {
+    $httpProvider.defaults.headers.common["X-CSRF-Token"] =
+      $("meta[name=csrf-token]").attr("content");
+  }
+]);
