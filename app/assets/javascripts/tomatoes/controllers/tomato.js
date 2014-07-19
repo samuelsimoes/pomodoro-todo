f55507e7da1ad20b5bb03a41e8863d56188692d3
Samuel Simões <slip.cm@gmail.com>
2014-07-19 19:57:37 -0300

    Edição de um pomodoro

--- app/assets/javascripts/tomatoes/controllers/tomato.js
+++ app/assets/javascripts/tomatoes/controllers/tomato.js
@@ -1,3 +1,18 @@
 angular.module("Pomodoro").controller("TomatoController", [
-  function () {}
+  "$scope",
+  function ($scope) {
+    $scope.editMode = false;
+
+    $scope.enterEditMode = function () {
+      $scope.editMode = true;
+    };
+
+    $scope.update = function () {
+      var promise = $scope.tomato.save();
+
+      promise.success(function () {
+        $scope.editMode = false;
+      });
+    };
+  }
 ]);
