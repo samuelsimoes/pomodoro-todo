0d088edf72d19c2591efb4c7a6047f602c94c8bc
Samuel Simões <slip.cm@gmail.com>
2014-07-26 18:31:19 -0300

    Tratamento para a exceção “ActiveRecord::RecordNotFound” na camada dos controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,21 @@ class ApplicationController < ActionController::Base
 
   self.responder = ApplicationResponder
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   def javascript_exists?
     script = "#{Rails.root}/app/assets/javascripts/#{controller_path}.js"
     File.exists?(script)
   end
   helper_method :javascript_exists?
+
+  protected
+
+  def not_found
+    if request.xhr?
+      render nothing: true, status: :not_found
+    else
+      redirect_to root_path
+    end
+  end
 end
