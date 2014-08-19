78871ee893977a4ebe090021eb2c298f0ad96911
Samuel Simões <slip.cm@gmail.com>
2014-08-19 10:52:26 -0300

    Carregando a primeira lista de pomodoros na index geral do aplicativo

--- app/assets/javascripts/pomodoro_lists/controllers/pomodoro_lists.js
+++ app/assets/javascripts/pomodoro_lists/controllers/pomodoro_lists.js
@@ -9,13 +9,19 @@ angular.module("Pomodoro").controller("PomodoroListsController", [
       pomodoreLists.add(new PomodoroList());
     };
 
-    pomodoreLists.fetch();
+    // show the first pomodoro list
+    pomodoreLists.fetch().then(function () {
+      if (!pomodoreLists.models) { return; }
+
+      $state.go("pomodoro_lists.show", {
+        pomodoroListId: pomodoreLists.models[0].attributes.id
+      });
+    });
 
     // return to index when the current viewed pomodoro list is exclued
     $scope.$on("pomodoro-list-deleted", function (evt, pomodoroList) {
-      if (pomodoroList.attributes.id == $state.params.pomodoroListId) {
-        $state.go("pomodoro_lists");
-      }
+      if (pomodoroList.attributes.id != $state.params.pomodoroListId) { return; }
+      $state.go("pomodoro_lists");
     });
   }
 ]);
